604cb64684f50e15f62b5970e866a239f545c8fa
kahfi <kahfi@vsi.co.id>
2020-12-08 09:42:00 +0700

    add FWCI 3.1.11

--- application/views/main.php
+++ application/views/main.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>BizLand Bootstrap Template - Index</title>
+  <title>Visindo Meta Data</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -58,18 +58,18 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="index.html">BizLand<span>.</span></a></h1>
+      <h1 class="logo mr-auto"><a href="index.html">PT. Visindo Meta Data<span>.</span></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt=""></a>-->
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li class="drop-down"><a href="">Drop Down</a>
+          <li><a href="#about">Tentang</a></li>
+          <li><a href="#services">Pelayanan</a></li>
+          <li><a href="#portfolio">Portofolio</a></li>
+          <li><a href="#team">Tim</a></li>
+          <!--li class="drop-down"><a href="">Drop Down</a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="drop-down"><a href="#">Deep Drop Down</a>
@@ -86,7 +86,7 @@
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#contact">Contact</a></li-->
 
         </ul>
       </nav><!-- .nav-menu -->
@@ -97,12 +97,13 @@
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex align-items-center">
     <div class="container" data-aos="zoom-out" data-aos-delay="100">
-      <h1>Welcome to <span>BizLand</spa>
+      <h1>Selamat Datang <!--span>BizLand</span-->
       </h1>
-      <h2>We are team of talanted designers making websites with Bootstrap</h2>
+      <!--h2>We are team of talanted designers making websites with Bootstrap</h2-->
+      <h2></h2>
       <div class="d-flex">
-        <a href="#about" class="btn-get-started scrollto">Get Started</a>
-        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox btn-watch-video" data-vbtype="video" data-autoplay="true"> Watch Video <i class="icofont-play-alt-2"></i></a>
+        <a href="#about" class="btn-get-started scrollto">Mulai</a>
+        <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox btn-watch-video" data-vbtype="video" data-autoplay="true"> Tonton Video <i class="icofont-play-alt-2"></i></a>
       </div>
     </div>
   </section><!-- End Hero -->
